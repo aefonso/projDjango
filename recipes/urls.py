7f77933df6773497ba35eb293d2ebcd66c4761ec
Afonso Henrique <89816770+aefonso@users.noreply.github.com>
2023-01-27 15:29:43 -0300

    feat: update

--- recipes/urls.py
+++ recipes/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('receitas3/', views.receitas3, name='receitas3'),
     path('receitas/', views.receitas, name='receitas'),
     path('contatos1/', views.contatos1, name='contatos1'),
-    path('contatos2/', views.contatos1, name='contatos2'),
-    path('contatos3/', views.contatos1, name='contatos3'),
+    path('contatos2/', views.contatos2, name='contatos2'),
+    path('contatos3/', views.contatos3, name='contatos3'),
 ]
