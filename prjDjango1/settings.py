0d89b96791c3b37c6d60b6eaedfa366fd8e8bf97
Afonso Henrique <89816770+aefonso@users.noreply.github.com>
2023-01-29 11:30:52 -0300

    feat: inserindo o endereço da página

--- prjDjango1/settings.py
+++ prjDjango1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2D585noucxsMXWTlYaQe7Xb2p2wyRaAscHdc1cRJRnXtisllyA0zndFkck4X'
+SECRET_KEY = '5dywz3WaervdXAybmQuRMqSDgP0x7NH1Ddclu6QpKxcd1FvTLik0ZhT7fJ8d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
